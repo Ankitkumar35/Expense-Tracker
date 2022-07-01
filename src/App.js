6325c79915102a9fe96f343f2d028339554bb664
Ankitkumar35 <56481469+Ankitkumar35@users.noreply.github.com>
2022-07-01 21:12:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import NewExpense from './Components/NewExpense/NewExpense';
+import Expenses from './Components/Expenses/Expenses';
+
+const DUMMY_EXPENSES = [
+  
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
